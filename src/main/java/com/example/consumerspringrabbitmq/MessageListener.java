263c3cb41fd4e75c4d954eeddde82c05d636ce09
Kisalay <kisalaykisu@gmail.com>
2022-09-25 14:41:53 +0530

    first commit

---
+++ src/main/java/com/example/consumerspringrabbitmq/MessageListener.java
@@ -0,0 +1,14 @@
+package com.example.consumerspringrabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void listener(CustomMessage message) {
+        System.out.println(message);
+    }
+
+}
---
+++ src/test/java/com/example/consumerspringrabbitmq/ConsumerSpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumerspringrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerSpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
